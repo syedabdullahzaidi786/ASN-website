805a8e7f47466c306b87f304fe431e90d24f580b
syedabdullahzaidi786 <ardevelopers622@gmail.com>
2025-01-09 01:23:49 +0500

    popup welcome message added

---
+++ script.js
@@ -0,0 +1,9 @@
+// Show popup when the page loads
+window.onload = function() {
+  document.getElementById("popup").style.display = "flex";
+}
+
+// Close the popup
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
